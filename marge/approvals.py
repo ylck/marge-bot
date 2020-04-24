e7d1f1523d131e49292cfdf1ed506e2567e54887
Lasse Mammen <lkm@bookboon.com>
2020-04-24 15:05:53 +0100

    Use CODEOWNERS and changes to determine who can approve
    At the moment all matches from codeowners has to be available to approve

--- marge/approvals.py
+++ marge/approvals.py
@@ -1,5 +1,7 @@
 import yaml
 import logging as log
+import shlex
+import fnmatch
 
 from . import gitlab
 
@@ -20,28 +22,65 @@ class Approvals(gitlab.Resource):
         if gitlab_version.is_ee:
             self._info = self._api.call(GET(approver_url))
         else:
-            self.get_approvers_ce()
+            self._info = self.get_approvers_ce()
 
     def get_approvers_ce(self):
         """get approvers status using thumbs on merge request
         """
 
-        config_file = self._api.repo_file_get(self.project_id, ".marge-bot.yml", "master")
-        if config_file is None:
-            log.info('Project id %s missing .marge-bot.yaml', self.project_id)
-            config = {}
-        else:
-            config = yaml.load(config_file["content"])
+        codeowners = self.determine_responsible_owners(self.get_codeowners_ce(), self.get_changes_ce())
+        awards = self.get_awards_ce()
 
+        up_votes = [e for e in awards if e['name'] == 'thumbsup' and e['user']['username'] in codeowners]
+        approver_count = len(codeowners)
+        approvals_left = max(approver_count - len(up_votes), 0)
+        return dict(self._info, approvals_left=approvals_left, approved_by=up_votes)
 
+    def determine_responsible_owners(self, owners_glob, changes):
+        owners = set([])
+
+        # Always add global users
+        if '*' in owners_glob:
+            owners.update(owners_glob['*'])
+
+        if 'changes' not in changes:
+            return owners
+
+        for change in changes['changes']:
+            for glob, users in owners_glob.items():
+                if 'new_path' in change and fnmatch.fnmatch(change['new_path'], glob):
+                    owners.update(users)
+
+        return owners
+
+    def get_changes_ce(self):
+        changes_url = '/projects/{0.project_id}/merge_requests/{0.iid}/changes'
+        changes_url = changes_url.format(self)
+
+        return self._api.call(GET(changes_url))
+
+    def get_awards_ce(self):
         emoji_url = '/projects/{0.project_id}/merge_requests/{0.iid}/award_emoji'
         emoji_url = emoji_url.format(self)
-        emoji = self._api.call(GET(emoji_url))
+        return self._api.call(GET(emoji_url))
 
-        up_votes = [e  for e in emoji if e['name'] == 'thumbsup']
-        approver_count = config.get('approver_count', 1)
-        approvals_left = max(approver_count - len(up_votes), 0)
-        self._info = dict(self._info, approvals_left=approvals_left, approved_by=up_votes)
+    def get_codeowners_ce(self):
+        config_file = self._api.repo_file_get(self.project_id, "CODEOWNERS", "master")
+        owner_globs = {}
+
+        if config_file is None:
+            return owner_globs
+
+        for line in config_file.splitlines():
+            if line != "" and not line.startswith(' ') and not line.startswith('#'):
+                elements = shlex.split(line) # line.split()
+                glob = elements.pop(0)
+                owner_globs.setdefault(glob, set([]))
+
+                for user in elements:
+                    owner_globs[glob].add(user.strip('@'))
+
+        return owner_globs
 
     @property
     def iid(self):
